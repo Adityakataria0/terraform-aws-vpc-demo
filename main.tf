0bce85484caa56babb0a9c2ba74da5d4a92c4872
Adityakataria0 <https://github.com/Adityakataria0>
2025-12-07 03:51:36 +0000

    Terraform VPC demo - initial commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = var.region
+}
+
+# -- Create VPC ---
+resource "aws_vpc" "main" {
+  cidr_block		= var.vpc_cidr
+  enable_dns_hostnames  = true
+
+  tags = {
+    Name = "tf-vpc-demo"
+  }
+}
+
+# --- Public Subnet ---
+resource "aws_subnet" "public" {
+  vpc_id		  = aws_vpc.main.id
+  cidr_block		  = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone	  = var.availability_zone
+
+  tags = {
+    Name = "tf-public-subnet"
+  }
+}
+
+# --- Internet Gateway ---
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "tf-igw"
+  }
+}
+
+# --- Route Table for Public Subnet ---
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "tf-public-route-table"
+  }
+}
+
+# --- Associate Route Table with Subnet ---
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
